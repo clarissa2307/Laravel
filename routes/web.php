b91c3515de7dde5e7bf79770556d92e70f11c423
clarissa2307 <marthaleo07@gmail.com>
2021-02-08 19:49:35 -0600

    vistas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', [UserController::class, 'index']);
-Route::post('users', [UserController::class, 'store']);
-Route::delete('users/{user}', [UserController::class, 'destroy']);
+Route::post('users', [UserController::class, 'store'])->name('users.store');
+Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
